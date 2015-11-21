61d5970982a19026985627dc95744516822c6e79
Vitalii Mostivskyi <vitaliymostivskiy@gmail.com>
2015-11-22 00:20:45 +0100

    Add maven project

---
+++ src/main/java/ua/vialii/mostivskyi/App.java
@@ -0,0 +1,13 @@
+package ua.vialii.mostivskyi;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Pryvit Ivan!" );
+    }
+}
