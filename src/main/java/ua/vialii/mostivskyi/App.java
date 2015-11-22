9979bbcb6305e9a3c16b6940257368a732f1955a
Vitalii Mostivskyi <vitaliymostivskiy@gmail.com>
2015-11-22 14:38:20 +0100

    Add for

--- src/main/java/ua/vialii/mostivskyi/App.java
+++ src/main/java/ua/vialii/mostivskyi/App.java
@@ -29,8 +29,11 @@ public class App
 
         }
         
-        
-        
-        
+        int i = 0;
+        for(;i < 10;)
+        {
+        	System.out.println(i);        	
+        	i++;
+        }
     }
 }
