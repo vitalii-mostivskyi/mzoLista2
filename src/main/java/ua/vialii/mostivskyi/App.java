7ff94e65783ffbb9d41bc01ea5ea9b12a9a18472
Vitalii Mostivskyi <vitaliymostivskiy@gmail.com>
2015-11-22 14:17:22 +0100

    switch added

--- src/main/java/ua/vialii/mostivskyi/App.java
+++ src/main/java/ua/vialii/mostivskyi/App.java
@@ -10,5 +10,24 @@ public class App
     {
         System.out.println( "Pryvit Ivan!" );
         System.out.println("new line!");
+        
+        int sw = 50;
+        
+        switch (sw)
+        {
+        case 1:
+        	System.out.println("one");
+        	break;
+        	
+        case 2:
+        	System.out.println("two");
+        	break;
+
+        case 1:
+        	System.out.println("three");
+        	break;
+
+        }
+        
     }
 }
